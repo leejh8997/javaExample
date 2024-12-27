518d08a06ef6b8afdee1b0fb814f1c802686ec05
tj-bu-708-PC19 <tj-bu-708-PC19@DESKTOP-HIMVBIM>
2024-12-27 18:08:05 +0900

    단축키

--- java_exam/src/memo/Note.java
+++ java_exam/src/memo/Note.java
@@ -1,7 +1,26 @@
 package memo;
 
 public class Note {
+	
+	// 정수(int)간의 나눗셈은 결과가 정수로 나온다.
+	//하나라도 실수(double)이면은 결과가 실수로 나온다.
+	
+	// 큰거 -> 작은거 (형변환)
+
 	//shift ent 커서가 해당열 아래로
 	//ctrl ent 문자와 커서가 해당열 아래로
 	//shift ctrl ent 커서는 해당열에 문자는 아래로
+	
+	// Ctrl + Shift + O => import 생성
+	
+	// Tab => 들여쓰기
+	// Shift + Tab => 내어쓰기(들여쓰기 반대)
+	// Ctrl + Space => 자동완성
+	// Ctrl + Shift + F => 자동들여쓰기
+	// syso(텍스트) + Ctrl + Space => 프린트문 자동 완성
+	// Ctrl + F11 => 실행
+	// Ctrl + / => 한줄 주석
+	// Ctrl + Shift + / => 여러줄 주석
+	// Ctrl + D => 한줄 삭제
+	// Alt + 위아래 화살표 => 코드 이동
 }
